1b6c0ede590053edcd6525e0477f27010efd36bd
antvega98 <139654398+antvega98@users.noreply.github.com>
2023-08-22 18:13:57 -0400

    Add args solution for 2703 [...].

---
+++ 2703-Return-Length-of-Arguments-Passed/args.js
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/return-length-of-arguments-passed/submissions/1029032446/
+
+/**
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
