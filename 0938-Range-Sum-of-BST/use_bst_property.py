94f9a78a3cbdefbb9a78a9a547d4550c3907514d
antvega98 <139654398+antvega98@users.noreply.github.com>
2023-08-29 21:05:06 -0400

    Add solution that uses bst property for 0938 Range Sum of BST.

---
+++ 0938-Range-Sum-of-BST/use_bst_property.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/range-sum-of-bst/submissions/1035534681/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        if root == None:
+            return 0
+
+
+        l = 0 if root.val < low else self.rangeSumBST(root.left, low, high)
+        r = 0 if root.val > high else self.rangeSumBST(root.right, low, high)
+
+        return l + r + (root.val if low <= root.val <= high else 0)
